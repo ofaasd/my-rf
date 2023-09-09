49ae4b7127ac0ad967723bd99e2b1e4f6dbcfb3f
ofaasd <ofaasd@gmail.com>
2023-09-09 19:30:34 +0700

    update pesan wa

--- application/views/admin/pembayaran/wa_form.php
+++ application/views/admin/pembayaran/wa_form.php
@@ -52,5 +52,35 @@ mohon diulang kembali
 </div>
 <?php 
 }else{
-	echo '<div class="alert alert-danger">Harap Velidasi Pembayaran Terlebih Dahulu</div>';
+?>
+<input type="hidden" name="id" id="id_pembayaran" value="<?= $pembayaran->id ?>">
+<div class="form-group">
+	<label for="nama">Nama</label>
+	<input type="text" name="nama" id="nama" class="form-control" value="<?= $nama_santri->nama?>">
+	
+	<label for="number">No Wa</label>
+	<input type="text" name="no_wa" id="no_wa" class="form-control" value="<?= $pembayaran->no_wa?>">
+	
+	<label for="pesan">Pesan</label>
+	<textarea name="message" class="form-control" rows="15" id="message">
+*Pesan ini otomatis dikirim dari sistem*
+Yth. (Bp/Ibu) <?=$pembayaran->atas_nama?>, telah melakukan melaporkan  pembayaran bulan september sebesar <?=number_format($pembayaran->jumlah,0,",",".")?> 
+
+dengan rincian sbb :
+
+<?php foreach($detail_pembayaran as $detail){?>
+	• <?=$jenis_pembayaran[$detail->id_jenis_pembayaran]?> sebesar Rp. <?=number_format($detail->nominal,0,",",".");?>&#13;
+<?php } ?>
+
+Kami mengucapkan banyak terima kasih (Bp/Ibu) <?=$pembayaran->atas_nama?> Yang telah melaporkan kepada kami.
+Tunggu beberapa waktu, kami akan melakukan pencatatan.
+Kami akan segera memberikan informasi apabila pembayaran tsb diatas telah sesuai.
+
+
+Semoga pekerjaan dan usahanya diberi kelancaran dan keberkahan menghasilkan Rizqi yang banyak dan berkah, aamiin. 
+
+Notifikasi ini bertujuan untuk menjaga amanah Bp/Ibu kepada kami. Bila ada yang perlu diklarifikasi mohon bisa menghubungi kami via WA atau telepon kami di nomor +62897-9194-645. Atau melalui https://saran.ppatq-rf.id
+	</textarea>
+</div>
+<?php
 } ?>
