3531908d13331ece968f62064ee6cb9dab7aeada
ofaasd <ofaasd@gmail.com>
2023-09-21 18:13:56 +0700

    adding echo

--- application/controllers/admin/Pembayaran.php
+++ application/controllers/admin/Pembayaran.php
@@ -615,6 +615,7 @@ K. Noor Shokhib M.Pd.I
 				$send_wa = $this->wa->send_wa($data);
 			}
 		}
+		echo $send_wa;
 		//redirect(base_url('index.php/admin/pembayaran/belum_lapor'));
 	}
 	public function kirim_ke_semua(){
