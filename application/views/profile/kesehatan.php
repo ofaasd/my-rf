3aa5f3c2e053aaad15e530265236d09d2e663088
ofaasd <ofaasd@gmail.com>
2024-02-11 00:35:24 +0700

    update riwayat sakit

--- application/views/profile/kesehatan.php
+++ application/views/profile/kesehatan.php
@@ -1,6 +1,29 @@
 <div class="card-header">
-        Form Edit Profile
-    </div>
-    <div class="card-content">
-	</div>
+        Kesehatan Santri
+</div>
+<div class="card-content">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<td>Tanggal Sakit</td>
+				<td>Jenis Sakit</td>
+				<td>Keterangan</td>
+				<td>Tanggal Sembuh</td>
+				<td>Keterangan</td>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+				foreach($data['riwayat_sakit'] as $row){
+					echo "<tr>";
+					echo "<td>" . date('d-m-Y', $row->tanggal_sakit) . "</td>";
+					echo "<td>" . $row->sakit . "</td>";
+					echo "<td>" . $row->keterangan_sakit . "</td>";
+					echo "<td>" . date('d-m-Y', $row->tanggal_sembuh) . "</td>";
+					echo "<td>" . $row->keterangan_sembuh . "</td>";
+					echo "</tr>";
+				}
+			?>
+		</tbody>
+	</table>
 </div>
