2548ef52cb540eb0bb19966d284ff959eb43d5fc
ofaasd <ofaasd@gmail.com>
2024-03-02 15:36:13 +0700

    update catatan camera

--- application/views/pembayaran/detail_pembayaran.php
+++ application/views/pembayaran/detail_pembayaran.php
@@ -199,7 +199,7 @@
 						<label for="bukti" class="form-label" style="margin:0">Upload Bukti Bayar</label>
 						<small id="passwordHelp" class="form-text text-muted">* Harap sertakan bukti bayar. <br />Jika tidak ada bukti bayar kami mohon maaf bila nanti tidak terkonversi / dianggap <br /> belum bayar, karena bukti bayar tidak ditemukan</small>
 						<input type="file" id="bukti" class="form-control col-md-12" name="bukti" required >
-						
+						<small class="form-text text-warning">Silahkan klik tombol diatas dan pilih camera untuk mengupload berkas langsung dari camera</small>
 					</div>
 
 					<div class="form-group">
