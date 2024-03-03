920f23d9e48637570353850b750b969949cc0e5d
ofaasd <ofaasd@gmail.com>
2024-03-03 08:32:10 +0700

    adding deleted is not null

--- application/controllers/Agenda.php
+++ application/controllers/Agenda.php
@@ -16,9 +16,9 @@ class Agenda extends CI_Controller {
 	public function index()
 	{
 		$tanggal = date('Y-m-d H:i:s');
-		$data['agenda_sekarang'] = $this->db->where('tanggal_mulai >= ', $tanggal)->where('tanggal_selesai <= ', $tanggal)->get('agenda')->result();
-		$data['agenda_akan_datang'] = $this->db->where('tanggal_mulai < ', $tanggal)->get('agenda')->result();
-		$data['agenda_lalu'] = $this->db->where('tanggal_selesai > ', $tanggal)->get('agenda')->result();
+		$data['agenda_sekarang'] = $this->db->where('tanggal_mulai <= ', $tanggal)->where('tanggal_selesai >= ', $tanggal)->where('deleted_at IS NULL', NULL, FALSE)->get('agenda')->result();
+		$data['agenda_akan_datang'] = $this->db->where('tanggal_mulai > ', $tanggal)->get('agenda')->where('deleted_at IS NULL', NULL, FALSE)->result();
+		$data['agenda_lalu'] = $this->db->where('tanggal_selesai < ', $tanggal)->get('agenda')->where('deleted_at IS NULL', NULL, FALSE)->result();
 		//exit;
 		$var['title'] = 'PPATQ Roudlotul Falah';
 		$var['content'] = $this->load->view('agenda/index',$data,true);
