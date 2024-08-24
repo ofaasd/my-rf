7796e4d91ea43c9d90e8822c07dc475c87a0e601
ofaasd <ofaasd@gmail.com>
2024-08-24 22:23:40 +0700

    update tanggal bayar

--- application/views/admin/pembayaran/wa_form.php
+++ application/views/admin/pembayaran/wa_form.php
@@ -114,10 +114,10 @@ Riwayat Pelaporan :
 		}
 		$tahun = date('Y');
 		$pembayaran = $this->db->where('MONTH(tanggal_bayar)',$new_bulan)->where('YEAR(tanggal_bayar)',$tahun)->where('validasi',1)->where('nama_santri',$nama_santri->no_induk)->where('is_hapus',0)->get('tb_pembayaran')->result();
-		echo $this->db->last_query();
+		//echo $this->db->last_query();
 		foreach($pembayaran as $row){
 			echo '*' . $bulan[$new_bulan] .'* ';
-			// echo $row->tanggal_bayar .' : Rp. ' . number_format($row->jumlah,0,',','.');
+			echo $row->tanggal_bayar .' : Rp. ' . number_format($row->jumlah,0,',','.');
 			print('\n');
 		}
 		//echo $new_bulan;
