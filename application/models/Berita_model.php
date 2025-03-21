29356571f30351d5bcf4f4fb1ccb8627b962337e
Dyaka Faiz <dyakafaiz123@gmail.com>
2025-03-21 14:24:03 +0700

    update enkripsi id berita

--- application/models/Berita_model.php
+++ application/models/Berita_model.php
@@ -4,7 +4,8 @@ Class Berita_model extends CI_Model{
     public function get_all(){
         $query = $this->db->where('berita.deleted_at', null)
                           ->get('berita');
-        return $query->result();
+        $results = $query->result();
+        return $results;
     }
 
     public function get_pagination($limit = 3, $offset = 0) {
@@ -15,7 +16,13 @@ Class Berita_model extends CI_Model{
         $this->db->order_by('berita.created_at', 'DESC');
         $this->db->limit($limit, $offset);
         $query = $this->db->get();
-        return $query->result();
+        $results = $query->result();
+
+        // Enkripsi ID dengan Base64
+        foreach ($results as $row) {
+            $row->id = rawurlencode(base64_encode($row->id . "ppatq"));
+        }
+        return $results;
     }
 
     public function get_latest() {
@@ -23,13 +30,22 @@ Class Berita_model extends CI_Model{
         $this->db->from('berita');
         $this->db->join('kategori_berita', 'berita.kategori_id = kategori_berita.id', 'left');
         $this->db->where('berita.deleted_at', null);
-        $this->db->order_by('berita.created_at', 'DESC'); // Urutkan berdasarkan kolom created_at dengan urutan menurun
-        $this->db->limit(3); // Ambil hanya satu data paling baru
+        $this->db->order_by('berita.created_at', 'DESC');
+        $this->db->limit(3);
         $query = $this->db->get();
-        return $query->result(); // Mengambil satu baris hasil
+        $results = $query->result();
+    
+        // Enkripsi ID dengan Base64
+        foreach ($results as $row) {
+            $row->id = rawurlencode(base64_encode($row->id . "ppatq"));
+        }
+    
+        return $results;
     }
 
-    public function get_by_id($id) {
+    public function get_by_id($idEnkripsi) {
+        $idDekripsi = base64_decode(rawurldecode($idEnkripsi));
+        $id = str_replace("ppatq", "", $idDekripsi);
         $query = $this->db->get_where('berita', array('id' => $id));
         return $query->row();
     }
